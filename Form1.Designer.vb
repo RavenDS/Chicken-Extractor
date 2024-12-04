1ca7757dfa008b221b5c4c6f5f6b7eb962ffbe38
RavenDS <115877793+RavenDS@users.noreply.github.com>
2024-12-04 04:03:10 +0100

    main upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,200 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        ArchivePathTextBox = New TextBox()
+        BrowseArchiveBtn = New Button()
+        OpenArchiveDialog = New OpenFileDialog()
+        SaveArchiveDialog = New SaveFileDialog()
+        SaveFolderDialog = New FolderBrowserDialog()
+        FolderPathTextBox = New TextBox()
+        SaveFolderBtn = New Button()
+        Label1 = New Label()
+        UnpackModeCombo = New ComboBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        LinkLabel1 = New LinkLabel()
+        Label4 = New Label()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Cursor = Cursors.Hand
+        Button1.Location = New Point(14, 322)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(236, 60)
+        Button1.TabIndex = 0
+        Button1.Text = "Unpack"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Cursor = Cursors.Hand
+        Button2.Location = New Point(270, 322)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(236, 60)
+        Button2.TabIndex = 1
+        Button2.Text = "Repack"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' ArchivePathTextBox
+        ' 
+        ArchivePathTextBox.Location = New Point(14, 138)
+        ArchivePathTextBox.Name = "ArchivePathTextBox"
+        ArchivePathTextBox.Size = New Size(422, 39)
+        ArchivePathTextBox.TabIndex = 2
+        ' 
+        ' BrowseArchiveBtn
+        ' 
+        BrowseArchiveBtn.Location = New Point(442, 138)
+        BrowseArchiveBtn.Name = "BrowseArchiveBtn"
+        BrowseArchiveBtn.Size = New Size(64, 39)
+        BrowseArchiveBtn.TabIndex = 3
+        BrowseArchiveBtn.Text = "..."
+        BrowseArchiveBtn.UseVisualStyleBackColor = True
+        ' 
+        ' OpenArchiveDialog
+        ' 
+        OpenArchiveDialog.FileName = "archive.dat"
+        ' 
+        ' FolderPathTextBox
+        ' 
+        FolderPathTextBox.Location = New Point(14, 248)
+        FolderPathTextBox.Name = "FolderPathTextBox"
+        FolderPathTextBox.Size = New Size(422, 39)
+        FolderPathTextBox.TabIndex = 4
+        ' 
+        ' SaveFolderBtn
+        ' 
+        SaveFolderBtn.Location = New Point(442, 248)
+        SaveFolderBtn.Name = "SaveFolderBtn"
+        SaveFolderBtn.Size = New Size(64, 39)
+        SaveFolderBtn.TabIndex = 5
+        SaveFolderBtn.Text = "..."
+        SaveFolderBtn.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(76, 33)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(89, 32)
+        Label1.TabIndex = 6
+        Label1.Text = "Mode: "
+        ' 
+        ' UnpackModeCombo
+        ' 
+        UnpackModeCombo.DropDownStyle = ComboBoxStyle.DropDownList
+        UnpackModeCombo.FormattingEnabled = True
+        UnpackModeCombo.Items.AddRange(New Object() {"CI/CI2/CI3/CI4", "CIU"})
+        UnpackModeCombo.Location = New Point(170, 30)
+        UnpackModeCombo.Name = "UnpackModeCombo"
+        UnpackModeCombo.Size = New Size(242, 40)
+        UnpackModeCombo.TabIndex = 7
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(14, 103)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(299, 32)
+        Label2.TabIndex = 8
+        Label2.Text = "Archive (.dat, .hq2x, .222x..)"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(14, 213)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(159, 32)
+        Label3.TabIndex = 9
+        Label3.Text = "Extract folder:"
+        ' 
+        ' LinkLabel1
+        ' 
+        LinkLabel1.AutoSize = True
+        LinkLabel1.Cursor = Cursors.Hand
+        LinkLabel1.Font = New Font("Segoe UI Semibold", 7.875F, FontStyle.Bold, GraphicsUnit.Point)
+        LinkLabel1.Location = New Point(170, 397)
+        LinkLabel1.Name = "LinkLabel1"
+        LinkLabel1.Size = New Size(92, 30)
+        LinkLabel1.TabIndex = 10
+        LinkLabel1.TabStop = True
+        LinkLabel1.Text = "ravenDS"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 7.875F, FontStyle.Italic, GraphicsUnit.Point)
+        Label4.Location = New Point(256, 397)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(71, 30)
+        Label4.TabIndex = 11
+        Label4.Text = "- 2024"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(518, 438)
+        Controls.Add(Label4)
+        Controls.Add(LinkLabel1)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(UnpackModeCombo)
+        Controls.Add(Label1)
+        Controls.Add(SaveFolderBtn)
+        Controls.Add(FolderPathTextBox)
+        Controls.Add(BrowseArchiveBtn)
+        Controls.Add(ArchivePathTextBox)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MaximizeBox = False
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "CI Extractor 1.1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents ArchivePathTextBox As TextBox
+    Friend WithEvents BrowseArchiveBtn As Button
+    Friend WithEvents OpenArchiveDialog As OpenFileDialog
+    Friend WithEvents SaveArchiveDialog As SaveFileDialog
+    Friend WithEvents SaveFolderDialog As FolderBrowserDialog
+    Friend WithEvents FolderPathTextBox As TextBox
+    Friend WithEvents SaveFolderBtn As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents UnpackModeCombo As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents Label4 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,298 @@
+﻿Imports System.IO
+Imports System.IO.Compression
+Imports System.Text
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If UnpackModeCombo.SelectedIndex = 0 Then
+            Dim inputFilePath As String = OpenArchiveDialog.FileName.ToString
+            Dim outputFolderPath As String = SaveFolderDialog.SelectedPath.ToString
+
+            If Not File.Exists(inputFilePath) Then
+                MessageBox.Show("Input archive file not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            If outputFolderPath = "" Then
+                'MessageBox.Show("No output folder selected!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                FolderPathTextBox.Text = Path.Combine(Path.GetDirectoryName(OpenArchiveDialog.FileName), Path.GetFileNameWithoutExtension(OpenArchiveDialog.FileName.ToString) & "-CIExtract")
+                outputFolderPath = SaveFolderDialog.SelectedPath.ToString
+            End If
+
+            If Not Directory.Exists(outputFolderPath) Then
+                Directory.CreateDirectory(outputFolderPath)
+            End If
+
+            Using fs As New FileStream(inputFilePath, FileMode.Open, FileAccess.ReadWrite)
+                Using br As New BinaryReader(fs)
+
+                    Dim fileCount As Integer = br.ReadInt32()
+                    Dim fileType As Integer = 0
+
+                    If fileCount <= 2 Then
+                        fileCount = br.ReadInt32()
+                        fileType = 1
+                    End If
+
+
+                    For i As Integer = 1 To fileCount
+                        Dim currentOffset As Long = fs.Position
+                        Dim testbyte As Byte = br.ReadByte()
+
+                        fs.Seek(currentOffset, SeekOrigin.Begin)
+                        Dim useXor As Boolean = (testbyte And &H80) <> 0
+                        Dim xorKey As Byte = &HCC
+
+                        If useXor Then
+                            ApplyXor(fs, currentOffset, xorKey, &H50)
+                        End If
+
+                        Dim rawName As Byte() = br.ReadBytes(&H50)
+                        Dim fileName As String = Encoding.ASCII.GetString(rawName).Split(Chr(0))(0).Trim()
+                        fileName = CleanPath(fileName)
+
+                        If useXor Then
+                            ApplyXor(fs, currentOffset, xorKey, &H50)
+                        End If
+
+                        Dim fileOffset As Integer = br.ReadInt32()
+                        Dim compressedSize As Integer = br.ReadInt32()
+                        Dim uncompressedSize As Integer = If(fileType = 0, compressedSize, br.ReadInt32())
+
+                        Dim fullFilePath As String = Path.Combine(outputFolderPath, fileName)
+                        Dim directoryPath As String = Path.GetDirectoryName(fullFilePath)
+
+                        If Not String.IsNullOrEmpty(directoryPath) AndAlso Not Directory.Exists(directoryPath) Then
+                            Directory.CreateDirectory(directoryPath)
+                        End If
+
+                        If uncompressedSize = compressedSize Then
+                            ExtractFile(fullFilePath, fs, fileOffset, compressedSize)
+                        Else
+                            ExtractFile(fullFilePath, fs, fileOffset, compressedSize, uncompressedSize)
+                        End If
+
+
+                    Next
+                End Using
+            End Using
+
+            MessageBox.Show("Unpacking complete!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        ElseIf UnpackModeCombo.SelectedIndex = 1 Then
+            MessageBox.Show("Chicken Invaders Universe is not supported yet", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("No unpack mode is selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+        ArchivePathTextBox.SelectionStart = ArchivePathTextBox.Text.Length
+        ArchivePathTextBox.SelectionLength = 0
+        FolderPathTextBox.SelectionStart = FolderPathTextBox.Text.Length
+        FolderPathTextBox.SelectionLength = 0
+
+    End Sub
+
+    Function CleanPath(filePath As String) As String
+        filePath = filePath.Replace("/", Path.DirectorySeparatorChar).Replace("\", Path.DirectorySeparatorChar)
+        Return String.Concat(filePath.Split(Path.GetInvalidPathChars())).Trim()
+    End Function
+
+    Sub ApplyXor(fs As FileStream, offset As Long, xorKey As Byte, length As Integer)
+        Dim originalPosition As Long = fs.Position
+        fs.Seek(offset, SeekOrigin.Begin)
+        Dim buffer(length - 1) As Byte
+        fs.Read(buffer, 0, length)
+
+        For i As Integer = 0 To buffer.Length - 1
+            buffer(i) = buffer(i) Xor xorKey
+        Next
+
+        fs.Seek(offset, SeekOrigin.Begin)
+        fs.Write(buffer, 0, buffer.Length)
+        fs.Seek(originalPosition, SeekOrigin.Begin)
+    End Sub
+
+    Sub ExtractFile(outputPath As String, fs As FileStream, offset As Integer, compressedSize As Integer, Optional uncompressedSize As Integer = -1)
+        Dim originalPosition As Long = fs.Position
+        fs.Seek(offset, SeekOrigin.Begin)
+
+        Dim buffer(compressedSize - 1) As Byte
+        fs.Read(buffer, 0, compressedSize)
+
+        File.WriteAllBytes(outputPath, buffer)
+
+        fs.Seek(originalPosition, SeekOrigin.Begin)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If UnpackModeCombo.SelectedIndex = 0 Then
+            Dim inputFilePath As String = OpenArchiveDialog.FileName.ToString
+            Dim outputFolderPath As String = SaveFolderDialog.SelectedPath.ToString
+
+            If Not Directory.Exists(outputFolderPath) Then
+                MessageBox.Show("Output folder not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            If Not File.Exists(inputFilePath) Then
+                MessageBox.Show("Input archive file not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Exit Sub
+            End If
+
+            File.Copy(inputFilePath, inputFilePath & ".CIExtractBackup")
+
+            Using fs As New FileStream(inputFilePath, FileMode.Open, FileAccess.ReadWrite)
+                Using br As New BinaryReader(fs)
+                    Using bw As New BinaryWriter(fs)
+
+                        Dim fileCount As Integer = br.ReadInt32()
+                        Dim fileType As Integer = 0
+
+                        If fileCount <= 2 Then
+                            fileCount = br.ReadInt32()
+                            fileType = 1
+                        End If
+
+                        For i As Integer = 1 To fileCount
+                            Dim currentOffset As Long = fs.Position
+                            Dim testbyte As Byte = br.ReadByte()
+
+                            fs.Seek(currentOffset, SeekOrigin.Begin)
+                            Dim useXor As Boolean = (testbyte And &H80) <> 0
+                            Dim xorKey As Byte = &HCC
+
+                            If useXor Then
+                                ApplyXor(fs, currentOffset, xorKey, &H50)
+                            End If
+
+                            Dim rawName As Byte() = br.ReadBytes(&H50)
+                            Dim fileName As String = System.Text.Encoding.ASCII.GetString(rawName).Split(Chr(0))(0).Trim()
+                            fileName = CleanPath(fileName)
+
+                            If useXor Then
+                                ApplyXor(fs, currentOffset, xorKey, &H50)
+                            End If
+
+                            Dim fileOffset As Integer = br.ReadInt32()
+                            Dim compressedSize As Integer = br.ReadInt32()
+                            Dim uncompressedSize As Integer = If(fileType = 0, compressedSize, br.ReadInt32())
+
+                            Dim fullFilePath As String = Path.Combine(outputFolderPath, fileName)
+
+                            If File.Exists(fullFilePath) Then
+                                Dim newFileBytes = File.ReadAllBytes(fullFilePath)
+                                Dim originalPosition As Long = fs.Position
+
+                                fs.Seek(fileOffset, SeekOrigin.Begin)
+                                Dim originalFileBytes(compressedSize - 1) As Byte
+                                fs.Read(originalFileBytes, 0, compressedSize)
+
+                                If Not newFileBytes.SequenceEqual(originalFileBytes) Then
+                                    Dim newSize As Integer = newFileBytes.Length
+                                    Dim sizeDifference As Integer = newSize - compressedSize
+
+                                    ' Rewrite header with updated size and adjust offsets if necessary
+                                    If sizeDifference > 0 Then
+                                        fs.Seek(0, SeekOrigin.End)
+                                        Dim newOffset As Integer = CInt(fs.Position)
+
+                                        fs.Seek(currentOffset + &H50, SeekOrigin.Begin)
+                                        bw.Write(newOffset)
+                                        bw.Write(newSize)
+                                        If fileType <> 0 Then
+                                            bw.Write(newSize) ' For uncompressed size
+                                        End If
+
+                                        ' Write new data at the end of the file
+                                        fs.Seek(newOffset, SeekOrigin.Begin)
+                                        fs.Write(newFileBytes, 0, newSize)
+                                    Else
+                                        ' Rewrite data in place if size is smaller or same
+                                        fs.Seek(fileOffset, SeekOrigin.Begin)
+                                        fs.Write(newFileBytes, 0, newSize)
+
+                                        ' Handle leftover bytes if size decreased
+                                        If sizeDifference < 0 Then
+                                            Dim padding(-sizeDifference - 1) As Byte
+                                            fs.Write(padding, 0, padding.Length)
+                                        End If
+
+                                        ' Update header with new size
+                                        fs.Seek(currentOffset + &H50, SeekOrigin.Begin)
+                                        bw.Write(fileOffset)
+                                        bw.Write(newSize)
+                                        If fileType <> 0 Then
+                                            bw.Write(newSize)
+                                        End If
+                                    End If
+                                End If
+
+                                fs.Seek(originalPosition, SeekOrigin.Begin)
+                            End If
+                        Next
+                    End Using
+                End Using
+            End Using
+            MessageBox.Show("Repacking complete!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        ElseIf UnpackModeCombo.SelectedIndex = 1 Then
+            MessageBox.Show("Chicken Invaders Universe is not supported yet", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Else
+            MessageBox.Show("No repack mode is selected", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Sub
+
+    Private Sub BrowseArchiveBtn_Click(sender As Object, e As EventArgs) Handles BrowseArchiveBtn.Click
+        If OpenArchiveDialog.ShowDialog = DialogResult.OK Then
+
+            ArchivePathTextBox.Text = OpenArchiveDialog.FileName.ToString
+
+            If SaveFolderDialog.SelectedPath = "" Then
+                FolderPathTextBox.Text = Path.Combine(Path.GetDirectoryName(OpenArchiveDialog.FileName), Path.GetFileNameWithoutExtension(OpenArchiveDialog.FileName.ToString) & "-CIExtract")
+            End If
+
+            ArchivePathTextBox.SelectionStart = ArchivePathTextBox.Text.Length
+            ArchivePathTextBox.SelectionLength = 0
+            FolderPathTextBox.SelectionStart = FolderPathTextBox.Text.Length
+            FolderPathTextBox.SelectionLength = 0
+
+        End If
+    End Sub
+
+    Private Sub SaveFolderBtn_Click(sender As Object, e As EventArgs) Handles SaveFolderBtn.Click
+        If SaveFolderDialog.ShowDialog = DialogResult.OK Then
+            FolderPathTextBox.Text = SaveFolderDialog.SelectedPath.ToString
+
+            FolderPathTextBox.SelectionStart = FolderPathTextBox.Text.Length
+            FolderPathTextBox.SelectionLength = 0
+
+        End If
+    End Sub
+
+    Private Sub ArchivePathTextBox_TextChanged(sender As Object, e As EventArgs) Handles ArchivePathTextBox.TextChanged
+        OpenArchiveDialog.FileName = ArchivePathTextBox.Text.ToString
+    End Sub
+
+    Private Sub FolderPathTextBox_TextChanged(sender As Object, e As EventArgs) Handles FolderPathTextBox.TextChanged
+        SaveFolderDialog.SelectedPath = FolderPathTextBox.Text.ToString
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UnpackModeCombo.SelectedIndex = 0
+    End Sub
+
+    Private Sub UnpackModeCombo_SelectedIndexChanged(sender As Object, e As EventArgs) Handles UnpackModeCombo.SelectedIndexChanged
+        If UnpackModeCombo.SelectedIndex = 1 Then
+            MessageBox.Show("Chicken Invaders Universe is not supported yet", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            UnpackModeCombo.SelectedIndex = 0
+        End If
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Dim url As String = "https://github.com/RavenDS/"
+        Process.Start(New ProcessStartInfo(url) With {.UseShellExecute = True})
+    End Sub
+End Class
